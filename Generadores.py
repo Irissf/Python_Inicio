838341070aa5c0c36c44f3466848762db0127c59
Irissf <iris.seijo@gmail.com>
2021-06-29 12:03:49 +0200

    Generadores

---
+++ Generadores.py
@@ -0,0 +1,24 @@
+def generaPares(limite):
+    num = 1
+    miLista = []
+
+    while num < limite:
+        miLista.append(num*2)
+        num = num + 1
+    return miLista
+
+print(generaPares(10))
+
+
+#Con un generador
+def generaParesConGenerador(limite):
+    num = 1
+
+    while num < limite:
+        yield num*2
+        num = num + 1
+
+devuelvePares=generaParesConGenerador(10)
+for i in devuelvePares:
+    print(i,end=("-"))
+    
